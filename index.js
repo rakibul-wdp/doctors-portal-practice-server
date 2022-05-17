946b0e0089759e2088859c3eee73810e17b7fcc9
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-18 05:46:31 +0600

    create get api for load data from mongodb

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log('Database connected');
+    const serviceCollection = client.db('doctors_practice').collection('services');
+
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
   } finally {
     // some type of code that stop this function
   }
